472fb48a8c8e74bcbb62a458970df286e73cd762
Lukasz112358 <73369140+Lukasz112358@users.noreply.github.com>
2023-02-01 03:31:00 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -9,67 +9,73 @@
 #include<vector>
 #include<bitset>
 #include<cassert>
-#include"field.h"
-#include"furier.h"
+//#include"field.h"
+#include"fourier.h"
 
+//using namespace std;
 
-typedef long long int LL;
-
-LL p = 7340033;//7 * 2^20 + 1
-LL psemilog2 = 20;
-LL pr = 2187;//(pr^n)%p = 1<=>2^psemilog|n 
-vector<field> compute(field l, field r, vector<field>& g, vector<field>& f){
-    cout<<l<<' '<<r<<endl;
+std::vector<field> compute(long long l, long long r, std::vector<field>& g, std::vector<field>& f){
+    std::cout<<l<<' '<<r<<std::endl;
     if(l < r){
-        LL m = (l+r)/2;
+        long long m = (l+r)/2;
         compute(l,m,g,f);
-        vector<LL>F(r-l+1);
-        vector<LL>G(m-l+1);
+        std::vector<field>F(r-l+1);
+        std::vector<field>G(m-l+1);
         for(int i = 0; i < r-l+1; i++){
             F[i] = i*f[i];
         }
         for(int i = 0; i < m-l+1; i++){
             G[i] = g[i+l];
         }
-        vector<LL>H = multiplication(F,G);
+        std::vector<field>H = multiplication(F,G);
         for(int i=(int)(m+1);i<r+1;i++){
-            g[i]= add(g[i],div1(H[i-l],i));
+            g[i]= g[i]+(H[i-l]/i);
         }
         compute(m+1,r,g,f);
     }
     return g;
 }
 
-vector<LL> mainCompute(LL t, vector<field>& f){
-    vector<LL> g(t+1,0);
+std::vector<field> mainCompute(long long t, std::vector<field>& f){
+    std::vector<field> g(t+1,0);
     g[0] = 1;
     return compute(0,t,g,f);
 }
 
 
 
-vector<LL> B(vector<LL>& s, LL n, LL t, vector<LL> ans){
-    vector<LL> a(t+1);
-    for(int i =0; i<n; i++){
-        if(s[i]<=t)a[s[i]] ++;
+std::vector<field> B(std::vector<field>& s, long long t, std::vector<field> ans){
+    std::vector<field> a(t+1);
+    for(int j=0;j<1000;j++)std::cout<<a[0];
+    for(int i =0; i<ans.size(); i++){
+        for(int j=0;j<1000;j++)std::cout<<i;
+        if(s[i]<=t)a[s[i].getValue()] ++;
     }
-    for(LL k = 1; k <= t; k++){
-        cout << k<<endl;
-        for(LL j = 1; j <= t/k ; j++){
-            ans[k*j] = add(ans[k*j] , div1(mul(a[k],power((-1),j-1)),j));
+    for(int i =0; i<10000;i++)std::cout << i<<std::endl;
+    for(long long k = 1; k <= t; k++){
+        std::cout << k<<std::endl;
+        for(long long j = 1; j <= t/k ; j++){
+            field x = field(-1);
+            ans[k*j] = ans[k*j] + a[k]*(x^(j-1))/field(j);
         }
     }
     return ans;
 }
 
-vector<LL> final(vector<LL>& s,LL n, LL t){
-    vector<LL> Bans(t+1) = B(s,n,t,Bans) ;
-    vector<LL> computeAns = mainCompute(t,Bans);
+std::vector<field> JinWu(std::vector<field>& s, long long t){
+    std::vector<field> Bans(t+1);
+    Bans= B(s,t,Bans) ;
+    std::vector<field> computeAns = mainCompute(t,Bans);
     return computeAns;
+
 }
 
 
+
 int main() {
-    test();
+    std::vector<field>S(5,0);
+    S[0]=3;
+    S[1]=112;
+    std::cout<<JinWu(S,15)[3]<<true;
     return  0;
 }
